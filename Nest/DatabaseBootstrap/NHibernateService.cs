05e001f3143befa7af65b25f32921cb6c6943e76
LAP-SOPAR\Luka <luka.sopar@fer.hr>
2019-01-11 14:24:12 +0100

    Dodan Session factory

--- Nest/DatabaseBootstrap/NHibernateService.cs
+++ Nest/DatabaseBootstrap/NHibernateService.cs
@@ -6,9 +6,26 @@ using System;
 
 namespace DatabaseBootstrap
 {
-    public class DatabaseBootstrap
+    public class NHibernateService
     {
-        public static ISession OpenSession()
+        private static ISessionFactory _sessionFactory;
+        public ISession OpenSession()
+        {
+            try
+            {
+                if (_sessionFactory == null)
+                {
+                    _sessionFactory = OpenSessionFactory();
+                }
+                ISession session = _sessionFactory.OpenSession();
+                return session;
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException ?? e;
+            }
+        }
+        public static ISessionFactory OpenSessionFactory()
         {
             var nhConfig = Fluently.Configure()
             .Database(SQLiteConfiguration.Standard
@@ -28,10 +45,10 @@ namespace DatabaseBootstrap
                 .AddFromAssemblyOf<Zivotinja>())
             .BuildConfiguration();
             var sessionFactory = nhConfig.BuildSessionFactory();
-            ISession sess = sessionFactory.OpenSession();
+            //ISession sess = sessionFactory.OpenSession();
             // var schemaExport = new SchemaExport(nhConfig);
             //schemaExport.Create(false, true);
-            return sess;
+            return sessionFactory;
         }
     }
 }
